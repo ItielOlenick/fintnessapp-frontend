d7c56a7f012bf2d5f96d788b76ae06fc9b193412
Itiel Olenick <etl676@gmail.com>
2021-07-26 20:57:04 +0300

    link to exercises

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
             <h2 className="primary-color">Fitness App</h2>
             <div>
                 <Link to="/" className="ms-3 link">Home</Link>
-                <Link to="#" className="ms-3 link">My Workouts</Link>
+                <Link to="/exercises" className="ms-3 link">Exercises</Link>
                 <Link to="#" className="ms-3 link">New Workout</Link>
                 <Link to="#" className="ms-3 link">About</Link>
             </div>
