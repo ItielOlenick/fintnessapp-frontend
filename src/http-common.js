e6c1eece47fc5277062f74f473187a2e616e55e9
Itiel Olenick <etl676@gmail.com>
2021-07-26 20:37:18 +0300

    somthing

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create(
     {
-        //baseURL: "http://localhost:8080/api",
+        //dev----> baseURL: "http://localhost:8080/api",
         baseURL: "https://fintnessapp-backend.herokuapp.com/api",
         headers: {
             "Content-type": "application/json"
