4e30061bfe675a25f3daab5727cef28f2c2a0cce
Itiel Olenick <etl676@gmail.com>
2021-07-26 12:55:17 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import UsersList from './components/UsersList';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NoteFound from './components/NotFound';
 
 function App() {
   return (
-    <div>
-      <UsersList/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path = "/" component={UsersList} />
+          <Route path="*" component={NoteFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+const NoteFound = () => {
+    return ( 
+        <div>
+            <p>The page you are looking for is not yet developed</p>
+        </div>
+     );
+}
+ 
+export default NoteFound;
