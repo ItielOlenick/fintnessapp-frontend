f2b1c3662ad34f1d756b11be098db300d9d313f0
Itiel Olenick <etl676@gmail.com>
2021-07-26 11:56:47 +0300

    1

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import UsersService from "../services/UsersService";
+
 
 const UsersList = () => {
 
-    const [users, setUsers] = useState([
-        {name: "John", email: "GG@gmail.com"}
-    ])
+    const [users, setUsers] = useState([]);
+
+useEffect(() => {
+    UsersService.getAll()
+    .then(response => {
+        console.log('printing response', response.data);
+        setUsers(response.data);
+    })
+    .catch(error => {
+        console.log("Error - somthing is wrond", error);
+    })
+}, []);
 
     return ( 
 
         <div>
             <h1>List of users</h1>
             {
-                users.map(user => (
+                users && users.map(user => (
                     <div>
                         <p>{user.name}</p>
                         <p>{user.email}</p>
