b95282225c9ca05339ccf3e49c08f8571378f9c5
Itiel Olenick <etl676@gmail.com>
2021-07-26 11:49:29 +0300

    updating files

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 const UsersList = () => {
+
+    const [users, setUsers] = useState([
+        {name: "John", email: "GG@gmail.com"}
+    ])
+
     return ( 
+
         <div>
             <h1>List of users</h1>
+            {
+                users.map(user => (
+                    <div>
+                        <p>{user.name}</p>
+                        <p>{user.email}</p>
+                    </div>
+                ))
+            }
         </div>
     );
 }
