9bd7815006991197f99d680632be987d77648a41
Itiel Olenick <etl676@gmail.com>
2021-07-30 17:38:50 +0300

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ const Navbar = () => {
     return (
         <div className="container-fluid">
         <nav className = "navbar navbar-expand-lg navbar-light">
-            <h2 className="primary-color">Fitness App</h2>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNavbar">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse flex-row-reverse" id="topNavbar">
+                <h2 className="primary-color">Fitness App</h2>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNavbar">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            <div className="collapse navbar-collapse  justify-content-end" id="topNavbar">
                 <div><Link to="/" className="ms-3 link">Home</Link></div>
                 <div><Link to="/exercises" className="ms-3 link">Exercises</Link></div>
                 <div><Link to="/addExercise" className="ms-3 link">Add Exercise</Link></div>
