b977a3c6900c3e2341bcfdcfddc14a43a8bb2066
Itiel Olenick <etl676@gmail.com>
2021-08-19 10:06:40 +0300

    fix

--- src/http-common.js
+++ src/http-common.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export default axios.create({
   //dev
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
 
   //production
-  // baseURL: "https://fintnessapp-backend.herokuapp.com/api",
-  // headers: {
-  //   "Content-type": "application/json",
-  // },
+  baseURL: "https://fintnessapp-backend.herokuapp.com/api",
+  headers: {
+    "Content-type": "application/json",
+  },
 });
