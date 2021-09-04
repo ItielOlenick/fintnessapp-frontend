f45faf2f897c2aecfd0bb01087186707f446f91c
Itiel Olenick <etl676@gmail.com>
2021-09-04 23:52:50 +0300

    qf

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // dev
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
 
   //for mobile purposes
   // baseURL: "http://192.168.1.174:8080/api",
@@ -10,8 +10,8 @@ export default axios.create({
   // baseURL: "http://10.0.0.22:8080/api",
 
   // production
-  // baseURL: "https://fintnessapp-backend.herokuapp.com/api",
-  // headers: {
-  //   "Content-type": "application/json",
-  // },
+  baseURL: "https://fintnessapp-backend.herokuapp.com/api",
+  headers: {
+    "Content-type": "application/json",
+  },
 });
